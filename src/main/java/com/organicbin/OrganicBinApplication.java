f68efb38b8f0896ebf5d3e82483dedcd24f4791f
kalp-vora <kalp.vora09@gmail.com>
2022-08-28 23:17:45 +0530

    Organic Bin Created

---
+++ src/main/java/com/organicbin/OrganicBinApplication.java
@@ -0,0 +1,13 @@
+package com.organicbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organicbin/OrganicBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organicbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
